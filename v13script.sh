934026f878cb666e3809d1cf3823e95e75c6b053
Envy <tanveergill022@gmail.com>
2022-08-05 15:00:51 +0530

    Removed Conda and using pyenv for virtual env (Bug fix)

--- v13script.sh
+++ v13script.sh
@@ -1,56 +1,56 @@
 #!/bin/bash
 
 #To delete frappe_bench folder if already installed to avoid error
-rm -rf frappe-bench
+# rm -rf frappe-bench
 
-#Download anaconda form anaconda.com    Note: update link if newer version available
-sudo apt-get update; sudo apt-get install make build-essential libssl-dev zlib1g-dev \
+#setting up utilities
+sudo apt-get update -y
+
+sudo apt-get install make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
-libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
 
-#virtual environment name can be anything without space and special character to use later commands like "conda activate myenv"
-echo "Enter name for virtual environment(without spaces) :"
-read VARNAME
-sleep 2
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 
-#Anaconda script will run to install anaconda.       TIP: Do "ctrl+c" and then "v" to skip agreement details if you don't want to read that
-chmod 755 Anaconda3-2022.05-Linux-x86_64.sh
-./Anaconda3-2022.05-Linux-x86_64.sh
-sleep 2
+cd ~/.pyenv && src/configure && make -C src
 
-#To activate (base) environment and use "conda" command
-source /home/$(whoami)/anaconda3/bin/activate
 
-#It will install right version of python in env which is 3.8.1
-conda create -n $VARNAME python=3.8.1
-sleep 2
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 
-#To activate your environment with python=3.8.1
-conda activate $VARNAME
+source ~/.bashrc
 
+exec "$SHELL"
+echo "Done till now"
+python -V
+
+pyenv install 3.8.1 
+
+cd
+
+pyenv shell 3.8.1
 #To check if version is right
 python --version
-sleep 5
 
-#It will install bench=5.8.1
+
+# #It will install bench=5.8.1
 pip3 install frappe-bench==5.8.1
 
-#It will install frappe v13 in "frappe_bench" folder
-bench init --frappe-branch v13.23.0 frappe-bench
-cd frappe-bench
+# #It will install frappe v13 in "frappe_bench" folder
+bench init --frappe-branch v13.23.0 frappe_v13
+cd frappe-bench_v13
+pyenv local 3.8.1
 
-#New site where erpnext v13 will be installed
+# #It will install erpnext v13 app in "frappe_bench" folder
+bench get-app erpnext --branch v13.23.0
+
+bench new-site $SITENAME
+bench use $SITENAME
 echo "Enter Domain name  (must not be registerd in mysql before)  :"
 read SITENAME
 
-#It will install erpnext v13 app in "frappe_bench" folder
-bench get-app erpnext --branch v13.23.0
-
-#Site is installing
-bench new-site $SITENAME
-bench use $SITENAME
-
-#Erp will install in site
+# #Erp will install in site
 bench --site $SITENAME install-app erpnext
 
 echo "Thanks for using script. Now use "bench start"   -Envy"
