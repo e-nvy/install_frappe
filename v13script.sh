c16dcb26c3f19ff198f42f03f2b5611bf8f9e7b9
Envy <tanveergill022@gmail.com>
2022-08-05 20:53:04 +0530

    added git in `to be install` list

--- v13script.sh
+++ v13script.sh
@@ -11,7 +11,7 @@ sudo apt clean
 sudo apt-get update -y
 sudo apt clean
 
-sudo apt-get install make build-essential libssl-dev zlib1g-dev \
+sudo apt-get install git make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev redis mariadb cron -y
 curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
