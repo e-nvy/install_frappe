6651d043c94490332bc03935355fd616fb12971e
Envy <tanveergill022@gmail.com>
2022-08-05 20:38:37 +0530

    Trying to fix ubuntu update cmd issue

--- v13script.sh
+++ v13script.sh
@@ -6,14 +6,16 @@
 
 #setting up utilities
 sudo apt clean
+sudo apt upgrade -y 
+sudo apt clean 
 sudo apt-get update -y
 sudo apt clean
-curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt-get install -y nodejs
+
 sudo apt-get install make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
-libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev redis mariadb yarn cron -y
-
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev redis mariadb cron -y
+curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs yarn
 sudo systemctl start mariadb
 sudo systemctl start redis
 sudo mysql_secure_installation
