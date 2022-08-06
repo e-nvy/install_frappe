1b681d2881efa2d95c25d33cb3ab74d88f684259
Envy <tanveergill022@gmail.com>
2022-08-06 14:36:17 +0530

    pre-final

--- v13script.sh
+++ v13script.sh
@@ -33,35 +33,23 @@ echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 
 . ~/.bashrc
-# exec "$SHELL"
-#echo "Done till now"
-#python -V
-# . ~/.bashrc
 pyenv install 3.8.1 
 cd
-# exec "$SHELL"
-. ~/.bashrc 
 pyenv shell 3.8.1
-#To check if version is right
+
 python --version
-sleep 5
-# #It will install bench=5.8.1
+
 pip3 install frappe-bench==5.8.1
 rm -rf frappe_v13
-# #It will install frappe v13 in "frappe_bench" folder
+
 bench init --frappe-branch v13.23.0 frappe_v13
 cd frappe-bench_v13
 
 pyenv local 3.8.1
-# #It will install erpnext v13 app in "frappe_bench" folder
 bench get-app erpnext --branch v13.23.0
 echo "Enter Domain name  (must not be registerd in mysql before)  :"
 read SITENAME
 bench new-site $SITENAME
 bench use $SITENAME
-
-
-# #Erp will install in site
-bench --site $SITENAME install-app erpnext
 
 echo "Thanks for using script. Now use "bench start"   -Envy"
